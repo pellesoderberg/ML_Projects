665d710c091676ef8d07a1c69fb38798825939d8
pellesoderberg <pellesoederberg@gmail.com>
2019-11-25 19:18:05 +0100

    Finished first project

--- PycharmProjects/iris/venv/iris_ex.py
+++ PycharmProjects/iris/venv/iris_ex.py
@@ -1,9 +1,6 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-from pandas import pd
-from pandas.plotting import scatter_matrix
+# Load libraries
+from pandas import read_csv
+from pandas.tools.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -18,24 +15,66 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-iris_dataset = load_iris()
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+#Data properties
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('class').size())
+
+#Plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+dataset.hist()
+pyplot.show()
+scatter_matrix(dataset)
+pyplot.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state=1)
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
 
-print("Shape of data: {}".format(iris_dataset['data'].shape))
-print("Target names: {}".format(iris_dataset['target_names']))
-print("Feature names: {}".format(iris_dataset['feature_names']))
-print("Type of data: {}".format(type(iris_dataset['data'])))
-print("Shape of data: {}".format(iris_dataset['data'].shape))
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
 
-import pandas as pd
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
 
-data = load_iris()
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df.head()
-#
-# print("Type of target: {}".format(type(iris_dataset['target'])))
-# print("Shape of target: {}".format(iris_dataset['target'].shape))
-# print("Target:\n{}".format(iris_dataset['target']))
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
 #
 # X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 #
