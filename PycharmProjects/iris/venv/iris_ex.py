91251f3a8b330839a20536a22c198e35fe13fd9a
Pelle Söderberg <pelso446@student.liu.se>
2019-11-25 15:25:58 +0100

    First commit

---
+++ PycharmProjects/iris/venv/iris_ex.py
@@ -0,0 +1,63 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from pandas import pd
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+iris_dataset = load_iris()
+
+
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: {}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+
+import pandas as pd
+
+data = load_iris()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df.head()
+#
+# print("Type of target: {}".format(type(iris_dataset['target'])))
+# print("Shape of target: {}".format(iris_dataset['target'].shape))
+# print("Target:\n{}".format(iris_dataset['target']))
+#
+# X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+#
+# print("X_train shape: {}".format(X_train.shape))
+# print("y_train shape: {}".format(y_train.shape))
+#
+# # Same for the test samples
+# print("X_test shape: {}".format(X_test.shape))
+# print("y_test shape: {}".format(y_test.shape))
+#
+# knn = KNeighborsClassifier(n_neighbors=1)
+#
+# knn.fit(X_train, y_train)
+#
+# X_new = np.array([[5, 2.9, 1, 0.2]])
+# print("X_new.shape: {}".format(X_new.shape))
+#
+# prediction = knn.predict(X_new)
+# print("Prediction: {}".format(prediction))
+# print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
+#
+# y_pred = knn.predict(X_test)
+# print("Test set predictions:\n {}".format(y_pred))
+# print("Test set score (np.mean): {:.2f}".format(np.mean(y_pred == y_test)))
+# print("Test set score (knn.score): {:.2f}".format(knn.score(X_test, y_test)))
